6c27ffb0c9783bb851b815db81616e08f933f368
Johnatan Brayan <johnatanbrayan1@gmail.com>
2020-12-04 23:56:33 -0300

    Improvement Panorama

--- js/Panorama360.js
+++ js/Panorama360.js
@@ -1,42 +1,49 @@
-let camera, scene, renderer;
 
 function init() {
-  scene = new THREE.Scene();
-  // camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+  
+  /* Configure Renderer */
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+  window.addEventListener( 'resize', onResize );
 
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  /* Renderer */
+  const scene = new THREE.Scene();
 
-  const controls = THREE.OrbitControls(camera, renderer.domElement);
-  // controls.rotateSpeed = 0.2;
+  /* Camera */
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  
+  /* Controls */
+  const controls = THREE.OrbitControls( camera, renderer.domElement );
+  
+  /* controls.update() must be called after any manual changes to the camera's transform */
+  camera.position.set(-1, 0, 0);
+  // camera.position.set(1, 0, 0);
   // controls.enableZoom = false;
-  camera.position.set(1, 0, 0);
+  // controls.rotateSpeed = 0.2;
   // controls.update();
 
-  // camera.setSize(window.innerWidth / window.innerHeight);
+  /* Object geometry */
+  const geometry = new THREE.SphereGeometry(50, 32, 32);
+  const textureLoader = new THREE.TextureLoader();
+  const texture = textureLoader.load('/img/360.jpeg');
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.repeat.x = -1;
+  const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+  const sphere = new THREE.Mesh(geometry, material);
 
-  let geometry = new THREE.SphereGeometry(50, 32, 32);
-  let textureLoader = new THREE.TextureLoader();
-  let texture = textureLoader.load('/img/360.jpeg');
-  let material = new THREE.MeshBasicMaterial({
-    map: texture,
-    side: THREE.DoubleSide
-  });
-  let sphere = new THREE.Mesh(geometry, material);
   scene.add(sphere);
-
-  function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-  }
 
   animate();
 
   function onResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
   }
-  window.addEventListener('resize', onResize);
+  
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
 }
