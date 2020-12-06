1fb6b04336cf0b02d308b949555df0d27a90c60c
Johnatan Brayan <johnatanbrayan1@gmail.com>
2020-12-06 19:34:37 -0300

    Improving Panorama360

--- js/Panorama360.js
+++ js/Panorama360.js
@@ -5,43 +5,47 @@ function init() {
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
-  window.addEventListener( 'resize', onResize );
 
-  /* Renderer */
+  /* Make window responsible */
+  window.addEventListener( 'resize', onWindowResize );
+
+  /* Scene */
   const scene = new THREE.Scene();
 
   /* Camera */
   const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  
-  /* Controls */
-  const controls = THREE.OrbitControls( camera, renderer.domElement );
-  
-  /* controls.update() must be called after any manual changes to the camera's transform */
   camera.position.set(-1, 0, 0);
-  // camera.position.set(1, 0, 0);
-  // controls.enableZoom = false;
-  // controls.rotateSpeed = 0.2;
-  // controls.update();
+  
+  /* Control */
+  const controls = new THREE.OrbitControls( camera, renderer.domElement );
+  controls.enableZoom = false;
+  controls.rotateSpeed = 0.41;
+  /* controls.update() must be called after any manual changes to the camera's transform */
+  controls.update();
 
   /* Object geometry */
   const geometry = new THREE.SphereGeometry(50, 32, 32);
   const textureLoader = new THREE.TextureLoader();
-  const texture = textureLoader.load('/img/360.jpeg');
+  const texture = textureLoader.load('/img/kandao3.jpg');
   texture.wrapS = THREE.RepeatWrapping;
   texture.repeat.x = -1;
   const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
   const sphere = new THREE.Mesh(geometry, material);
 
+  /* Add scene */
   scene.add(sphere);
 
+  /* Rendering the scene */
   animate();
 
-  function onResize() {
+  /* Method to resize windows automatically */
+  function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
   }
   
+  /* Method to Rendering the scene */
   function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
