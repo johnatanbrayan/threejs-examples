cde9ea2d6b0007ec5afd60a85f493b1632f09f2a
Johnatan Brayan <johnatanbrayan1@gmail.com>
2020-12-07 13:38:05 -0300

    Organizing code simple-cube

--- js/SimpleCube.js
+++ js/SimpleCube.js
@@ -1,51 +1,54 @@
-var scene, camera, renderer;
-
 function init() {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  renderer = new THREE.WebGLRenderer();
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  window.addEventListener('resize', function() {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-  });
-
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-  /* Create the shape */
-  var geometry = new THREE.BoxGeometry(1, 1, 1);
-
-  /* Create a material, color or image texture */
-  var material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, wireframe: false });
-  var cube = new  THREE.Mesh(geometry, material);
-  scene.add(cube);
-
+  /* Configure Renderer */
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+  
+  /* Make window responsible */
+  window.addEventListener( 'resize', onResizeWindow );
+  
+  /* Scene */
+  const scene = new THREE.Scene();
+  
+  /* Camera */
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
   camera.position.z = 3;
 
+  /* Control */
+  const controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+  /* Create the shape */
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+
+  /* Create a material, color or image texture */
+  const material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, wireframe: false });
+  const cube = new  THREE.Mesh( geometry, material );
+
+  /* Add Scene */
+  scene.add( cube );
+
+  /* Run Game Loop */
+  gameLoop();
+
+  /* Run Game Loop ( update, render, repeat ) */
+  function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    update();
+    /* Draw Scene */
+    renderer.render(scene, camera);
+  };
+
   /* Game Logic */
-  var update = function() {
+  function update() {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.005; 
   };
 
-  /* Draw Scene */
-  var render = function() {
-    renderer.render(scene, camera);
-  };
-
-  /* Run Game Loop (update, render, repeat) */
-  var gameLoop = function() {
-    requestAnimationFrame(gameLoop);
-
-    update();
-    render();
-  };
-
-  gameLoop();
+  /* Method to resize window automatically */
+  function onResizeWindow() {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  }
 }
