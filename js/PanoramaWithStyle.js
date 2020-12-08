b22733921143e956cbaa85f7eba58959b133dbb4
Johnatan Brayan <johnatanbrayan1@gmail.com>
2020-12-07 22:59:56 -0300

    Insering new project

--- js/PanoramaWithStyle.js
+++ js/PanoramaWithStyle.js
@@ -1,11 +1,11 @@
 function init() {
-  
+
   /* Configure Renderer */
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
-  /* Make window responsible */
+  /* Resize panorama with window */
   window.addEventListener( 'resize', onWindowResize );
 
   /* Scene */
@@ -13,39 +13,46 @@ function init() {
 
   /* Camera */
   const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  camera.position.set( -1, 0, 0 );
-  
+  camera.position.set( 1, 0.2, 0.8 );
+
   /* Control */
   const controls = new THREE.OrbitControls( camera, renderer.domElement );
   controls.enableZoom = false;
   controls.rotateSpeed = 0.41;
-  /* controls.update() must be called after any manual changes to the camera's transform */
   controls.update();
 
-  /* Object geometry */
+  /* Object Geometry */
   const geometry = new THREE.SphereGeometry( 50, 32, 32 );
-  const texture = new THREE.TextureLoader().load( '/img/kandao3.jpg' );
+  const texture = new THREE.TextureLoader().load( '/img/360-4.jpg' );
   texture.wrapS = THREE.RepeatWrapping;
   texture.repeat.x = -1;  
   const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
   const sphere = new THREE.Mesh( geometry, material );
 
-  /* Add scene */
+  /* Add Scene */
   scene.add( sphere );
 
-  /* Rendering the scene */
+  /* Tooltip */
+  const spriteMap = new THREE.TextureLoader().load( '/img/info-icon.png' );
+  const spriteMaterial = new THREE.SpriteMaterial({ map: spriteMap });
+  const sprite = new THREE.Sprite( spriteMaterial );
+  const position = new THREE.Vector3( -4.8, -1, -13 );
+  sprite.position.copy( position );
+  scene.add( sprite );
+
+  /* Rendering the Scene */
   animate();
 
-  /* Method to resize windows automatically */
-  function onWindowResize() {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-  }
-  
   /* Method to rendering the scene */
   function animate() {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
+  }
+
+  /* Method to resize panorama with window */
+  function onWindowResize() {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
   }
 }
