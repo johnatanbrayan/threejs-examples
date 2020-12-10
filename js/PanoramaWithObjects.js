ac49e59542d36d99db6f41428f7ac18a35e60b3e
Johnatan Brayan <johnatanbrayan1@gmail.com>
2020-12-09 21:25:42 -0300

    Refactoring code PanoramaWithObjects

---
+++ js/PanoramaWithObjects.js
@@ -0,0 +1,122 @@
+let tooltipActive = false;
+
+function init() {
+
+  /* Configure Renderer */
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  /* Get div with class tooltip on html */
+  const tooltip = document.querySelector('.tooltip');
+  
+  /* Method to get event when click on button icon */
+  document.body.addEventListener( 'click', onClick );
+
+  /* Method to get event when move mouse on icon */
+  document.body.addEventListener( 'mousemove', onMouseMove );
+  
+  /* Resize panorama with window */
+  window.addEventListener( 'resize', onWindowResize );
+
+  /* Scene */
+  const scene = new THREE.Scene();
+
+  /* treats images */
+  const rayCaster = new THREE.Raycaster();
+
+  /* Camera */
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  camera.position.set( 1, 0.2, 0.8 );
+
+  /* Control */
+  const controls = new THREE.OrbitControls( camera, renderer.domElement );
+  controls.enableZoom = false;
+  controls.rotateSpeed = 0.41;
+  controls.update();
+
+  /* Object Geometry */
+  const geometry = new THREE.SphereGeometry( 50, 32, 32 );
+  const texture = new THREE.TextureLoader().load( '/content/img360/kandao3.jpg' );
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.repeat.x = -1;  
+  const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+  const sphere = new THREE.Mesh( geometry, material );
+
+  /* Add Scene */
+  scene.add( sphere );
+
+  /* Rendering the Scene */
+  animate();
+
+  /* Add Tooltip */
+  addTooltip( new THREE.Vector3( -18.44473765293672, -5.421965978565057, -46.072338908811915 ), 'Informação' );
+
+  /* Method Tooltip */
+  function addTooltip( position, name ) {
+    let spriteMap = new THREE.TextureLoader().load( '/content/icon/info-icon.png' );
+    let spriteMaterial = new THREE.SpriteMaterial({ map: spriteMap });
+    let sprite = new THREE.Sprite( spriteMaterial );
+    sprite.name = name;
+    // const position = new THREE.Vector3( -4.8, -1, -13 );
+    sprite.position.copy( position.clone().normalize().multiplyScalar(30) );
+    sprite.scale.multiplyScalar( 2.1 );
+    scene.add( sprite );
+  }
+
+  /* Method to set action when move mouse on button icon */
+  function onMouseMove(e) {
+    let foundSprite = false;
+    let mouse = new THREE.Vector2( ( e.clientX / window.innerWidth ) * 2 - 1, - ( e.clientY / window.innerHeight ) * 2 + 1,);
+
+    rayCaster.setFromCamera( mouse, camera );
+    let intersects = rayCaster.intersectObjects( scene.children );
+    intersects.forEach( ( intersect ) => {
+      if ( intersect.object.type === 'Sprite' ) {
+        let p = intersect.object.position.clone().project( camera );
+        tooltip.style.top = ( ( -1 * p.y + 1 ) * window.innerHeight / 2 ) + 'px';
+        tooltip.style.left = ( ( p.x + 1 ) * window.innerWidth / 2) + 'px';
+        tooltip.classList.add('is-active');
+        tooltipActive = true;
+        foundSprite = true;
+      }
+    });
+
+    if ( foundSprite === false && tooltipActive ) {
+      tooltip.classList.remove('is-active');
+    }
+  }
+  
+  /* Method to set action when click button icon */
+  function onClick(e) {
+    let mouse = new THREE.Vector2( ( e.clientX / window.innerWidth ) * 2 - 1, - ( e.clientY / window.innerHeight ) * 2 + 1 );
+    
+    rayCaster.setFromCamera( mouse, camera );
+    let intersects = rayCaster.intersectObjects( scene.children );
+    intersects.forEach( ( intersect ) => {
+      if ( intersect.object.type === 'Sprite' ) {
+        console.log( intersect.object.name );
+      }
+    });
+    /**
+    let intersects = rayCaster.intersectObject( sphere );
+    if ( intersects.length > 0 ) {
+      console.log( intersects[0].point );
+      addTooltip( intersects[0].point );
+    }
+    **/
+  }
+
+  /* Method to rendering the scene */
+  function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+  }
+
+  /* Method to resize panorama with window */
+  function onWindowResize() {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  }
+}
